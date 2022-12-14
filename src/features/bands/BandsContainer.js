38ebac35c85afe02418cc43770550388fb6e848d
Hillary Muller <hillaryaward@gmail.com>
2022-12-14 14:36:22 -0600

    commit tree e68da3cc8e0a033944dcfd143ce1dab4c802a466

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import BandInput from './BandInput';
+import { bandAdded } from './bandsSlice';
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const bands = useSelector((state) => state.bands.entities);
+  const dispatch = useDispatch();
+  function handleBandSubmit(name) {
+    dispatch(bandAdded(name));
+  }
+  console.log(bands);
+  const bandsList = bands.map((band, index) => <li key={index}>{band}</li>)
+  return (
+    <div>
+      <BandInput onBandSubmit={handleBandSubmit} />
+      <ul>{bandsList}</ul>
+  </div>
+);
 }
 
 export default BandsContainer;
